332ae7c6e4500c57544a560052cd42b5d9816ffb
matamorosjose <jmatamorossantos@my.wctc.edu>
2021-09-02 18:37:07 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day3_class_git_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
